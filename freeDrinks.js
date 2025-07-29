1738168e41f8a83e6e3fbb2b26feb590f47b89c9
sujoncodes <sujonkumar.dev1@gmail.com>
2025-07-29 16:56:26 +0600

    ternary operator

--- freeDrinks.js
+++ freeDrinks.js
@@ -30,23 +30,44 @@
 
 // console.log(Results);
 
-const myScore = 78;
-const fndScore = 35;
-let Do;
-if(myScore >= 80){
-    if(fndScore >=80){
-        Do = "Let's go for lunch."
-    }else if(fndScore >=60){
-        Do = "Good luck next time"
-    }else if(fndScore >= 40){
-        Do = "Never seen his message"
-    } else {
-        Do = "Block him"
-    }
-    }
+// ___________________ Friend score _________________________
 
-else{
-    Do = "I am going to home & act sad."
+// const myScore = 78;
+// const fndScore = 35;
+// let Do;
+// if(myScore >= 80){
+//     if(fndScore >=80){
+//         Do = "Let's go for lunch."
+//     }else if(fndScore >=60){
+//         Do = "Good luck next time"
+//     }else if(fndScore >= 40){
+//         Do = "Never seen his message"
+//     } else {
+//         Do = "Block him"
+//     }
+//     }
+
+// else{
+//     Do = "I am going to home & act sad."
+// }
+// console.log(Do);
+
+
+// _____________________ Ternary Operator _____________________
+// const num1 = 20;
+// const num2 = 25;
+// let i;
+// i = num1>num2 ? num1*2: num1 + num2;
+// console.log(i)
+
+
+// _____________________ Normal if else _______________________
+const num1 = 50;
+const num2 = 95;
+let i;
+if(num1 > num2){
+    i = num1*2
+}else{
+    i = num1 + num2
 }
-console.log(Do);
-
+console.log(i)
