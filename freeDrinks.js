b4394b1765359875dd0bcb4dad27f5faae7a37dd
sujoncodes <sujonkumar.dev1@gmail.com>
2025-07-29 15:43:35 +0600

    Results Grade

--- freeDrinks.js
+++ freeDrinks.js
@@ -1,9 +1,46 @@
-let burgerPrice = 400;
-let drinks = 30;
+// let burgerPrice = 400;
+// let drinks = 30;
 
-if(burgerPrice >= 500){
-    console.log("Borh you can get a free drinks")
+// if(burgerPrice >= 500){
+//     console.log("Borh you can get a free drinks")
+// }
+// else{
+//     console.log("Buy coke: 30tk")
+// };
+
+// const marks = 90;
+// let Results;
+
+// if(marks >= 90 && marks <= 100){
+//     Results = "Grade-A"
+// } else if(marks >= 80){
+//     Results = "Grade-B"
+// } else if(marks >= 70){
+//     Results = "Grade-C"
+// } else if(marks >= 60){
+//     Results = "Grade-D"
+// } else{
+//     Results = "Grade-F"
+// }
+// console.log(Results);
+
+
+
+const marks = 999;
+let Results;
+
+if(marks > 100 || marks < 0){
+    Results = "invalid"
+} else if (marks >= 90 && marks <= 100) {
+    Results = "Grade-A";
+} else if (marks >= 80) {
+    Results = "Grade-B";
+} else if (marks >= 70) {
+    Results = "Grade-C";
+} else if (marks >= 60) {
+    Results = "Grade-D";
+} else {
+    Results = "Grade-F";
 }
-else{
-    console.log("Buy coke: 30tk")
-};
+
+console.log(Results);
