9dc0769f52b53638ea664700cdcccc5b6082ce7b
sujoncodes <sujonkumar.dev1@gmail.com>
2025-07-29 16:32:04 +0600

    Friend score

--- freeDrinks.js
+++ freeDrinks.js
@@ -8,39 +8,45 @@
 //     console.log("Buy coke: 30tk")
 // };
 
-// const marks = 90;
+
+
+
+// const marks = 999;
 // let Results;
 
-// if(marks >= 90 && marks <= 100){
-//     Results = "Grade-A"
-// } else if(marks >= 80){
-//     Results = "Grade-B"
-// } else if(marks >= 70){
-//     Results = "Grade-C"
-// } else if(marks >= 60){
-//     Results = "Grade-D"
-// } else{
-//     Results = "Grade-F"
+// if(marks > 100 || marks < 0){
+//     Results = "invalid"
+// } else if (marks >= 90 && marks <= 100) {
+//     Results = "Grade-A";
+// } else if (marks >= 80) {
+//     Results = "Grade-B";
+// } else if (marks >= 70) {
+//     Results = "Grade-C";
+// } else if (marks >= 60) {
+//     Results = "Grade-D";
+// } else {
+//     Results = "Grade-F";
 // }
+
 // console.log(Results);
 
+const myScore = 78;
+const fndScore = 35;
+let Do;
+if(myScore >= 80){
+    if(fndScore >=80){
+        Do = "Let's go for lunch."
+    }else if(fndScore >=60){
+        Do = "Good luck next time"
+    }else if(fndScore >= 40){
+        Do = "Never seen his message"
+    } else {
+        Do = "Block him"
+    }
+    }
 
-
-const marks = 999;
-let Results;
-
-if(marks > 100 || marks < 0){
-    Results = "invalid"
-} else if (marks >= 90 && marks <= 100) {
-    Results = "Grade-A";
-} else if (marks >= 80) {
-    Results = "Grade-B";
-} else if (marks >= 70) {
-    Results = "Grade-C";
-} else if (marks >= 60) {
-    Results = "Grade-D";
-} else {
-    Results = "Grade-F";
+else{
+    Do = "I am going to home & act sad."
 }
+console.log(Do);
 
-console.log(Results);
